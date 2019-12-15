92f55a1ee8d03e8e7dc98af7f79f942c2298e7eb
ingramKing <ingramSoar@163.com>
2019-12-16 07:18:51 +0800

    HiWord 3rd committed by ingramKing

---
+++ src/main/java/com/ingram/maven/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.ingram.maven;
+
+/**
+ * @ProjectName: HiWorld
+ * @ClassName: HelloWorld
+ * @Description: TODO
+ * @Author: Ingram
+ * @Date: 2019-12-16-07.06.42
+ * @Version: 1.0
+ **/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Every One!");
+    }
+}
