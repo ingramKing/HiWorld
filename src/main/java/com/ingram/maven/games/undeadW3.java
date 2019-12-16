6b8fc29873561a7f71530d4a717f85f771cf0e1e
ingramKing <ingramSoar@163.com>
2019-12-16 13:52:30 +0800

    HiWord 6th committed by ingramKing

---
+++ src/main/java/com/ingram/maven/games/undeadW3.java
@@ -0,0 +1,19 @@
+package com.ingram.maven.games;
+
+/**
+ * @ProjectName: HiWorld
+ * @ClassName: undeadW3
+ * @Description: TODO
+ * @Author: Ingram
+ * @Date: 2019-12-16-13.30.28
+ * @Version: 1.0
+ **/
+
+public class undeadW3 {
+
+    public String getGhoulDesc(){
+        return "食尸鬼：食尸鬼算是不死族最基本的单位，而且也算是低成本高输出的一个很好用的单位，在三本科技得到食尸鬼狂暴升级之后的单位则更加的疯狂。然而这个单位的耐性很差，所以大多数不死族一般不会把它作为主要部队来使用。(不适用与对战暗夜精灵)\n" +
+                "\n" +
+                "  采集木头的食尸鬼数量在正常游戏中为3-4个，这个数量取决于玩家期待在游戏中升几级攻防。一般来说在对抗暗夜精灵的情况下由于往往要打经济战所以4个是比较不错的选择，而对战不死族和人族时由于是恰timing，所以3个通常就够了。";
+    }
+}
